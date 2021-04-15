b323fe1714d32a7ea24af52d49e1de5a1a63e737
Santiago8888 <santiago.marti67@gmail.com>
2021-04-15 14:20:42 -0500

    Embed Medium Story.

--- scripts/analysis.ts
+++ scripts/analysis.ts
@@ -1,10 +1,12 @@
-// npx ts-node recommend
+// npx ts-node analysis
 
 
 import * as use from '@tensorflow-models/universal-sentence-encoder'
 import * as tf from '@tensorflow/tfjs-node'
-
+import { Tensor2D } from '@tensorflow/tfjs-node';
 import { PCA } from 'ml-pca'
+
+import { stories, iStory } from './init'
 
 
 const sentences = [
@@ -16,8 +18,7 @@ const sentences = [
     'Eating strawberries is healthy.'
 ];
 
-
-use.load().then(async model => {
+const demo = () => use.load().then(async model => {
     const embeddings = await model.embed(sentences)
     embeddings.print(true); // verbose
 
@@ -27,6 +28,49 @@ use.load().then(async model => {
     
     const pca = new PCA(dataset);
     console.log('dataset:', pca.predict(dataset, {nComponents:2}));
-});
+})
 
 
+
+interface iEmbeddedStory extends iStory {
+    embeddings: {
+        title: number[],
+        subtitle: number[],
+        tags: number[][],
+        topics: number[][],
+        paragraphs: number[][]
+    }
+}
+
+const analyzeStories = async() => {
+    const model = await use.load()
+    const numerize = (embeddings: Tensor2D) => [...Array(embeddings.shape[0])].reduce((d, i, idx) => 
+        [...d, Array.from(tf.slice(embeddings, [idx, 0], [1]).dataSync())], []
+    )
+
+
+    const embedStory = async(story: iStory): Promise<iEmbeddedStory> => {
+        const title = await model.embed(story.title)
+        const subtitle = await model.embed(story.subtitle)
+
+        const tags = await model.embed(story.tags)
+        const topics = await model.embed(story.topics)
+        const paragraphs = await model.embed(story.paragraphs)
+
+        return {
+            ...story,
+            embeddings:{
+                title: numerize(title)[0],
+                subtitle: numerize(subtitle)[0],
+                tags: numerize(tags),
+                topics: numerize(topics),
+                paragraphs: numerize(paragraphs)
+            }
+        }
+    }
+
+    const story = await embedStory(stories[0])
+    console.log('Story', story)
+}
+
+analyzeStories().then(console.log)
