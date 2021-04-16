c6b4737d239e02f1bdcaf4089c97aa78e595910e
Santiago8888 <santiago.marti67@gmail.com>
2021-04-16 02:26:25 -0500

    Fetch Debug Tweets.

--- cortazar/src/App.tsx
+++ cortazar/src/App.tsx
@@ -1,12 +1,38 @@
+import { App as RealmApp, User, Credentials } from "realm-web"
+import React, { useState, useEffect } from 'react'
 import { NavBar } from './components/NavBar'
 import { Stories } from './components/Grid'
+
+import debugTweets from './data/tweets.json'
+import amplitude from 'amplitude-js'
 
 import 'bulma/css/bulma.css'
 import './App.css'
 
-export const App = () => <>
-	<NavBar />
-	<div className="section" style={{padding:'1.5rem' }}>
-		<Stories stories={[]}/>
-	</div>
-</>
+const DEBUG = true
+
+
+const analyzeTweets = (tweets:string[]) => {}
+const recommendStories = (tweets:string[]) => {}
+
+export const App = () => {
+	const [ stories, setStories ] = useState()
+    const [ oauthToken, setOauthToken ] = useState('')
+
+    useEffect(() => {
+        if (DEBUG) {
+			analyzeTweets(debugTweets)
+			recommendStories(debugTweets)
+
+			return
+        }
+
+    }, [])
+
+	return <>
+		<NavBar />
+		<div className="section" style={{padding:'1.5rem' }}>
+			<Stories stories={[]}/>
+		</div>
+	</>
+}
