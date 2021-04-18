c49ab19ba64f15caa85b807ac2db6f0b3393f932
Santiago8888 <santiago.marti67@gmail.com>
2021-04-18 04:04:59 -0500

    Generalized message on Loading Screen.

--- cortazar/src/views/Loading.tsx
+++ cortazar/src/views/Loading.tsx
@@ -2,6 +2,6 @@ import 'bulma-pageloader/dist/css/bulma-pageloader.min.css'
 
 export const Loading = () =>  <div className={`pageloader is-active is-black`}>
     <span className="title" style={{textAlign:'center'}}>
-        Loading Data
+        Loading...
     </span>
 </div>
