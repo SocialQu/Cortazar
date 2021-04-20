ae92b9dca2df5b9e6ad7a05e3025229879fef870
Santiago8888 <santiago.marti67@gmail.com>
2021-04-19 21:26:16 -0500

    Stories grid with new types.

--- cortazar/src/components/Grid.tsx
+++ cortazar/src/components/Grid.tsx
@@ -1,6 +1,6 @@
+import { iStory, iStoryCard } from '../types/stories'
 import React, { useEffect, useState } from 'react'
-import { Story, iStoryCard } from './Story'
-import { iStory } from '../types/stories'
+import { Story } from './Story'
 
 
 const Row = ({ stories }: { stories: iStoryCard[] }) => <div className="columns">
@@ -10,13 +10,13 @@ const Row = ({ stories }: { stories: iStoryCard[] }) => <div className="columns"
 
 const centerDelta = ((a:number[], {center:b}:iStory) => Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]))
 
-interface iMaxScore {claps:number, topics:number, recommends:number, social:number, responses:number}
-const getScore = (story:iStory, {claps, topics, recommends, social, responses}: iMaxScore) => Math.round((
-    (story.claps/claps || 0)
-    + (story.topics.length/topics || 0)
-    + (story.recommends/recommends || 0)
-    + (story.socialRecommends/social || 0)
-    + (story.responses/responses || 0)
+interface iMaxScore { maxClaps:number, maxTopics:number, maxRecommends:number, maxSocial:number, maxResponses:number}
+const getScore = ({ stats, topics }:iStory, maxScore: iMaxScore) => Math.round((
+    (stats.claps/maxScore.maxClaps || 0)
+    + (topics.length/maxScore.maxTopics || 0)
+    + (stats.recommends/maxScore.maxRecommends || 0)
+    + (stats.socialRecommends/maxScore.maxSocial || 0)
+    + (stats.responses/maxScore.maxResponses || 0)
 )*20)
 
 export const Stories = ({ stories, center }: { stories:iStory[], center:number[] }) => {
@@ -25,19 +25,19 @@ export const Stories = ({ stories, center }: { stories:iStory[], center:number[]
     useEffect(() => {
         if(!stories.length) return
 
-        const claps = stories.reduce((d, {claps: i}) => d > i ? d : i, 0)
-        const topics = stories.reduce((d, {topics: i}) => d > i.length ? d : i.length, 0)
-        const recommends = stories.reduce((d, {recommends: i}) => d > i ? d : i, 0)
-        const social = stories.reduce((d, {socialRecommends: i}) => d > i ? d : i, 0)
-        const responses = stories.reduce((d, {responses: i}) => d > i ? d : i, 0)
+        const stats = stories.map(({ stats }) => stats)
+        const maxClaps = stats.reduce((d, {claps: i}) => d > i ? d : i, 0)
+        const maxTopics = stories.reduce((d, {topics: i}) => d > i.length ? d : i.length, 0)
+        const maxRecommends = stats.reduce((d, {recommends: i}) => d > i ? d : i, 0)
+        const maxSocial = stats.reduce((d, {socialRecommends: i}) => d > i ? d : i, 0)
+        const maxResponses = stats.reduce((d, {responses: i}) => d > i ? d : i, 0)
+        const maxScore = { maxClaps, maxTopics, maxRecommends, maxSocial, maxResponses }
 
-        const ratedStories:iStoryCard[] = stories.map(s => ({
-            title: s.title,
-            subtitle: s.subtitle,
-            image: s.imageURL,
-            intro: s.paragraphs.filter(p => p!== s.title && p !== s.subtitle),
-            match: centerDelta(center, s),
-            score: getScore(s, {claps, topics, recommends, social, responses})
+        const ratedStories:iStoryCard[] = stories.map(story => ({
+            ...story,
+            intro: story.intro.filter(p => p!== story.title && p !== story.subtitle),
+            match: centerDelta(center, story),
+            score: getScore(story, maxScore)
         }))
 
 
