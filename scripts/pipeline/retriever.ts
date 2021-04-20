2b7d310dbcb48f71cd4834b05514e1f91537758f
Santiago8888 <santiago.marti67@gmail.com>
2021-04-20 14:33:00 -0500

    Retrieve Stories from first 10 topics.

--- scripts/pipeline/retriever.ts
+++ scripts/pipeline/retriever.ts
@@ -12,14 +12,26 @@ const lexem = '])}while(1);</x>'
 topics.map(topic => console.log())
 const saveStories = async(topic: string) => {
     const url = `${root}/${topic}?format=json`
-    console.log('url', url)
 
     const { data, status } = await axios.get(url)
     const text = data.replace('])}while(1);</x>', '')
 
     await fs.writeFile(`./data/stories/${topic}.json`, text)
+    console.log(status, url)
     return status
 }
 
 
-saveStories(topics[0]).then(console.log).catch(console.log)
+// saveStories(topics[0]).then(console.log).catch(console.log)
+
+
+export const sleep = (secs: number) => new Promise(resolve => setTimeout(resolve, secs*1000))
+
+const retrieveStories = async() => {
+    for (const s of topics) {
+        await saveStories(s)
+        await sleep(10)
+    }
+} 
+
+//retrieveStories().then().catch(console.log)
