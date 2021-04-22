bdb88b8073ca7ae3cde59e7eaa81950ea8eac29a
Santiago8888 <santiago.marti67@gmail.com>
2021-04-22 01:34:01 -0500

    Parse, center & save stories in a single script.

--- scripts/pipeline/index.ts
+++ scripts/pipeline/index.ts
@@ -1,10 +1,20 @@
 // npx ts-node pipeline
 
+import * as use from '@tensorflow-models/universal-sentence-encoder'
+
 import { writeStories } from './retriever'
+import { centerStories } from './analysis'
+import { parseStories } from './parser'
+import { saveStories } from './storer'
+
 import Topics from '../data/topics.json'
+import { MongoClient } from 'mongodb'
 import { promises as fs } from 'fs'
+require('dotenv').config()
+
 
 const dir = `data/stories/${new Date().toString().slice(0,15)}`
+const uri = `mongodb+srv://${process.env.mongo_admin}/${process.env.cortazar_db}`
 
 const pipeline = async(start:number, end:number) => {
     try { await fs.access(dir) }
@@ -13,14 +23,24 @@ const pipeline = async(start:number, end:number) => {
     const topics = Topics.slice(start, end)
     console.log('Topics', topics)
 
+    const model = await use.load()
+
+    const client = new MongoClient(uri, { useUnifiedTopology: true })
+    await client.connect()
+    console.log('Connected')
+
     for (const t of topics) {
         const rawStories = await writeStories(t)
         await fs.writeFile(`${dir}/test.json`, rawStories)
+
+        const parsedStories = parseStories(rawStories)
+        const stories = await centerStories({stories:parsedStories, model})
+        await saveStories(stories, client)
     }
 
-
+    client.close()
     console.log('Success')
 }
 
 
-pipeline(0, 1).catch(console.log)
+pipeline(20, 21).catch(console.log)
