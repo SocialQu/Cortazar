17244e107f998a61f4d2fd3ba6a1769f85b67915
Santiago8888 <santiago.marti67@gmail.com>
2021-04-22 16:37:41 -0500

    Prepare reader for incoportation to pipeline.

--- scripts/pipeline/reader.ts
+++ scripts/pipeline/reader.ts
@@ -3,15 +3,13 @@
 import { iReferences } from './parser'
 import { promises as fs } from 'fs'
 
-interface iStoryFile {
+export interface iStoryFile {
     payload: {
         references: iReferences
     }
 }
 
-const path = 'data/stories'
-let files:iStoryFile[] = []
-const getStories = async(path:string, files:iStoryFile[]) => {
+export const readStories = async(path:string, files:iStoryFile[]):Promise<iStoryFile[]> => {
     const names = await fs.readdir(path)
 
     for (const name of names) {
@@ -20,13 +18,9 @@ const getStories = async(path:string, files:iStoryFile[]) => {
             const stories:iStoryFile = JSON.parse(file.toString())
             files = [...files, stories]
         } catch(e){ 
-            files = await getStories(`${path}/${name}`, files)
+            files = await readStories(`${path}/${name}`, files)
         }
     }
 
     return files
 }
-
-getStories(path, files)
-.then(files => console.log(files.length))
-.catch(console.log)
