4db2d9f5e23ca9d7e0a578d36fec56bed6489697
Santiago8888 <santiago.marti67@gmail.com>
2021-04-22 16:38:39 -0500

    Add support to retrain stories on pipeline.

--- scripts/pipeline/index.ts
+++ scripts/pipeline/index.ts
@@ -2,6 +2,7 @@
 
 import * as use from '@tensorflow-models/universal-sentence-encoder'
 
+import { iStoryFile, readStories } from './reader'
 import { centerStories } from './analysis'
 import { getStories } from './retriever'
 import { parseStories } from './parser'
@@ -16,26 +17,42 @@ require('dotenv').config()
 const dir = `data/stories/${new Date().toString().slice(0,15)}`
 const uri = `mongodb+srv://${process.env.mongo_admin}/${process.env.cortazar_db}`
 
-const pipeline = async(start:number, end:number) => {
-    try { await fs.access(dir) }
-    catch(e) { await fs.mkdir(dir) }
-
-    const topics = Topics.slice(start, end)
-    console.log('Topics', topics)
-
+interface iFetch { start:number, end:number }
+interface iPipeline { fetch?:iFetch, train?:boolean }
+const pipeline = async({fetch, train}:iPipeline) => {
     const model = await use.load()
 
     const client = new MongoClient(uri, { useUnifiedTopology: true })
     await client.connect()
     console.log('Connected')
 
-    for (const topic of topics) {
-        const rawStories = await getStories(topic)
-        await fs.writeFile(`${dir}/${topic}.json`, JSON.stringify(rawStories))
+    if(train){
+        const path = 'data/stories'
+        let files:iStoryFile[] = []
+        const storyFiles = await readStories(path, files)
 
-        const parsedStories = parseStories(rawStories.payload.references)
-        const stories = await centerStories({stories:parsedStories, model})
-        await saveStories(stories, client)
+        for (const { payload } of storyFiles) {
+            const parsedStories = parseStories(payload.references)
+            const stories = await centerStories({stories:parsedStories, model})
+            await saveStories(stories, client)
+        }        
+    }
+
+    else if(fetch){
+        try { await fs.access(dir) }
+        catch(e) { await fs.mkdir(dir) }
+    
+        const topics = Topics.slice(fetch.start, fetch.end)
+        console.log('Topics', topics)
+
+        for (const topic of topics) {
+            const rawStories:iStoryFile = await getStories(topic)
+            await fs.writeFile(`${dir}/${topic}.json`, JSON.stringify(rawStories))
+    
+            const parsedStories = parseStories(rawStories.payload.references)
+            const stories = await centerStories({stories:parsedStories, model})
+            await saveStories(stories, client)
+        }
     }
 
     client.close()
@@ -43,4 +60,5 @@ const pipeline = async(start:number, end:number) => {
 }
 
 
-pipeline(19, 20).catch(console.log)
+const fetch:iFetch = { start:9, end: 10 }
+pipeline({ train:true, fetch }).catch(console.log)
