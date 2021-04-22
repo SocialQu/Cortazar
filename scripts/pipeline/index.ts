76fd6f964b34f04bebe049ba3610910d1e3d4747
Santiago8888 <santiago.marti67@gmail.com>
2021-04-22 01:02:24 -0500

    Retrieve selected  topics, and fetch stories.

--- scripts/pipeline/index.ts
+++ scripts/pipeline/index.ts
@@ -1,17 +1,26 @@
 // npx ts-node pipeline
 
+import { writeStories } from './retriever'
+import Topics from '../data/topics.json'
 import { promises as fs } from 'fs'
-
 
 const dir = `data/stories/${new Date().toString().slice(0,15)}`
 
-const pipeline = async() => {
-    try { await fs.access(dir) } 
+const pipeline = async(start:number, end:number) => {
+    try { await fs.access(dir) }
     catch(e) { await fs.mkdir(dir) }
 
-    await fs.writeFile(`${dir}/test.json`, JSON.stringify({}))
+    const topics = Topics.slice(start, end)
+    console.log('Topics', topics)
+
+    for (const t of topics) {
+        const rawStories = await writeStories(t)
+        await fs.writeFile(`${dir}/test.json`, rawStories)
+    }
+
+
     console.log('Success')
 }
 
 
-pipeline().catch(console.log)
+pipeline(0, 1).catch(console.log)
