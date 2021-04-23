14bebd5a9927a483b5e6d7eb1a9c3fc64b51c041
Santiago8888 <santiago.marti67@gmail.com>
2021-04-22 19:50:36 -0500

    Add aggregation pipeline to recommend function.

--- scripts/pipeline/recommender.ts
+++ scripts/pipeline/recommender.ts
@@ -1,7 +1,8 @@
-// npx ts-node recommender
+// npx ts-node pipeline/recommender
 
 import * as use from '@tensorflow-models/universal-sentence-encoder'
 import PCA_Model from '../../cortazar/src/scripts/pca.json'
+import { iStory } from '../../cortazar/src/types/stories'
 import { PCA, IPCAModel } from 'ml-pca'
 import { vectorize } from './analysis'
 import { MongoClient } from 'mongodb'
@@ -17,12 +18,23 @@ const recommend = async(tweet: string) => {
     const pca = PCA.load(PCA_Model as IPCAModel)
     const vector = vectorize(embedding)
     const center = pca.predict(vector, {nComponents:2}).getRow(0)
+    console.log('center', center)
 
     const client = new MongoClient(uri, { useUnifiedTopology: true })
     await client.connect()
 
+    const Stories = client.db("Cortazar").collection("stories")
 
+    const geoNear = { $geoNear: { near:center, distanceField:'distance', }}
+    const limit = { $limit: 100 }
+    const stories: iStory[] = await Stories.aggregate([geoNear, limit]).toArray()
 
+    console.log('stories', stories.length)
+    console.log(stories[0].title, stories[0].subtitle)
 
+    await client.close()
 }
 
+
+recommend('Tesla Solar + Powerwall battery enables consumers to be their own utility')
+.catch(console.log)
