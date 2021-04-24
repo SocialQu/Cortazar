45fa4b6ed55016ce50bc2f2e7253944ae8595129
Santiago8888 <santiago.marti67@gmail.com>
2021-04-24 15:25:08 -0500

    Responsive Landing Site.

--- cortazar/src/views/Landing.tsx
+++ cortazar/src/views/Landing.tsx
@@ -1,34 +1,49 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
+import { useMediaQuery } from 'react-responsive'
+import { useState } from 'react'
 
-import React, { useState } from 'react'
+const Title = () => {
+    const largeScreen = useMediaQuery({ query: '(min-width: 900px)' })
 
-const title = 'What do you want to read today?'
-const Title = () => <div style={{ margin:'2rem' }}>
-    <p className='title is-1 has-text-centered' style={{color:'white'}}> { title } </p>
-    <p className='subtitle is-4 has-text-centered' style={{color:'darkorange' }}> 
-        Discover personalized reading recommendations <br/>
-        based on what you tweet, or crave.
-    </p>
-</div>
-
+    return <div style={{ marginBottom:'2em', marginTop:'2em' }}>
+        <p className='title is-1 has-text-centered' style={{color:'white'}}> 
+            What do you want to read{largeScreen ? ' today' : ''}?
+        </p>
+        {
+            largeScreen && 
+            <p 
+                className='subtitle is-4 has-text-centered' 
+                style={{color:'darkorange', marginBottom:'3em' }}
+            > 
+                Discover personalized reading recommendations <br/>
+                based on what you tweet, or crave.
+            </p>
+        }
+    </div>
+}
 
 const SearchBox = ({ demo }: { demo(tweet:string):void }) => {
     const [tweet, setTweet] = useState<string>('')
 
-    return <div style={{ margin:'5rem' }}>
-        <div className='control' style={{ width:560, margin:'auto' }}>
+    return <div style={{ maxWidth:'100%' }}>
+        <div className='control' style={{ margin:'auto' }}>
             <textarea 
                 value={tweet}
                 className='textarea'
+                style={{maxWidth:560, minWidth:'auto', margin:'auto'}}
                 onChange={({ target: { value }}) => setTweet(value)}
                 onKeyPress={({ key }) => key === 'Enter' ? demo(tweet) : null}
-                placeholder='Paste a tweet, or write a sentence about something you want to read...' 
+                placeholder='Paste a tweet, or write a sentence to find the best stories...' 
             />
         </div>
 
-        <div className='control' style={{ textAlign:'center', marginTop:'2em' }}>
-            <a className='button is-info' style={{width:560}} onClick={() => demo(tweet)}> Try it Free </a>
+        <div className='control' style={{ textAlign:'center', marginTop:'1em' }}>
+            <a 
+                className='button is-info' 
+                onClick={() => demo(tweet)} 
+                style={{width:'100%', maxWidth:560}
+            }> Try it Free </a>
         </div>
     </div>
 }
