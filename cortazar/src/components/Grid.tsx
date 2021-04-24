5139a74d8ac0b88aa849d666e5f0927323e651f5
Santiago8888 <santiago.marti67@gmail.com>
2021-04-24 14:44:45 -0500

    Add media queries on Grid.

--- cortazar/src/components/Grid.tsx
+++ cortazar/src/components/Grid.tsx
@@ -1,18 +1,26 @@
 import { filterStories } from '../scripts/filters'
+import { useMediaQuery } from 'react-responsive'
 import { Filters } from '../molecules/Filters'
 import { iStoryCard } from '../types/stories'
+import { Story, MobileStory } from './Story'
+
 import { useEffect, useState } from 'react'
-import { Story } from './Story'
 
 
-const Row = ({ story }: { story: iStoryCard }) => <div className='columns'>
-    <div className='column'> 
-        <Story {...story}/> 
+const Row = ({ story }: { story: iStoryCard }) => {
+    const isMobile = useMediaQuery({ query: '(max-width:: 768px)' })
+
+    return <div className='columns'>
+        <div className='column'> 
+            { !isMobile ? <Story {...story}/> : <MobileStory {...story}/>} 
+        </div>
     </div>
-</div>
+}
 
 
 export const Stories = ({ stories, search }: { stories:iStoryCard[], search:string }) => {
+    const isDesktop = useMediaQuery({ query: '(min-width:: 1024px)' })
+
     const [ storyCards, setStoryCards ] = useState<iStoryCard[]>([])
     const [ deactivate, setDeactivate ] = useState(false)
 
@@ -36,7 +44,7 @@ export const Stories = ({ stories, search }: { stories:iStoryCard[], search:stri
             <i style={{color:'lightskyblue'}}> "{ search }" </i>
         </h1>
 
-        <Filters topics={[]} tags={[]} deactivate={deactivate} filterStories={handleFilters}/>
+        { isDesktop && <Filters topics={[]} tags={[]} deactivate={deactivate} filterStories={handleFilters}/> }
         { storyCards.filter((_, i) => i < 10).map((story, i) => <Row story={story} key={i}/>) }
     </div>
 }
