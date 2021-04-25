d28f6d35f521eaffa0d12fe553487c0a18c2ac89
Santiago8888 <santiago.marti67@gmail.com>
2021-04-25 00:35:44 -0500

    Simplified Filters without Active.

--- cortazar/src/molecules/Filters.tsx
+++ cortazar/src/molecules/Filters.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Filter } from '../atoms/Filter'
 
 const readingTimeFilters = ['Short (< 5 minutes)', 'Medium (5 - 10 minutes)', 'Long (> 10 minutes)']
@@ -9,65 +9,28 @@ export type Filters = 'Topics' | 'Tags' | 'Rating' | 'Reading Time' | 'Sort'
 interface iFilters { 
     topics: string[]
     tags: string[]
-    deactivate:boolean
     filterStories(filter:Filters, item:string):void 
 }
 
 
-export const StoryFilters = ({topics, tags, deactivate, filterStories}: iFilters) => {
-    const [active, setActive] = useState<Filters>()
-    useEffect(() => { setActive(undefined) }, [deactivate])
-
-    const setFilterActive = (filter:Filters) => setActive(active !== filter ? filter : undefined)
-    return <div className='columns'>
-        <div className='column'>
-            <Filter
-                name={'Topics'}
-                isActive={active==='Topics'}
-                setActive={() =>  setFilterActive('Topics')}
-                filters={topics}
-                select={topic => filterStories('Topics', topic)}
-            />
-        </div>
-
-        <div className='column'>
-            <Filter 
-                name={'Tags'}
-                isActive={active==='Tags'}
-                setActive={() => setFilterActive('Tags')}
-                filters={tags}
-                select={tag => filterStories('Tags', tag)}
-            />
-        </div>
-
-        <div className='column'>
-            <Filter
-                name={'Rating'}
-                isActive={active==='Rating'}
-                setActive={() => setFilterActive('Rating')}
-                filters={ratingFilters}
-                select={rating => filterStories('Rating', rating)}
-            />
-        </div>
-
-        <div className='column'>
-            <Filter
-                name={'Reading Time'}
-                isActive={active==='Reading Time'}
-                setActive={() => setFilterActive('Reading Time')}
-                filters={readingTimeFilters}
-                select={time => filterStories('Reading Time', time)}
-            />
-        </div>
-
-        <div className='column'>
-            <Filter
-                name={'Sort'}
-                isActive={active==='Sort'}
-                setActive={() => setFilterActive('Sort')}
-                filters={sortFilters}
-                select={sorting => filterStories('Sort', sorting)}
-            />
-        </div>
+export const StoryFilters = ({topics, tags, filterStories}: iFilters) => <div className='columns'>
+    <div className='column'>
+        <Filter name={'Topics'} filters={topics} select={topic => filterStories('Topics', topic)} />
     </div>
-}
+
+    <div className='column'>
+        <Filter name={'Tags'} filters={tags} select={tag => filterStories('Tags', tag)} />
+    </div>
+
+    <div className='column'>
+        <Filter name={'Rating'} filters={ratingFilters} select={rating => filterStories('Rating', rating)} />
+    </div>
+
+    <div className='column'>
+        <Filter name={'Reading Time'} filters={readingTimeFilters} select={time => filterStories('Reading Time', time)} />
+    </div>
+
+    <div className='column'>
+        <Filter name={'Sort'} filters={sortFilters} select={sorting => filterStories('Sort', sorting)}/>
+    </div>
+</div>
