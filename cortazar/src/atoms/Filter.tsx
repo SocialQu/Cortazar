bfdb34f6d19ccc798f8b5dcc2e542130fb920795
Santiago8888 <santiago.marti67@gmail.com>
2021-04-25 00:28:16 -0500

    Change Active to Hoverable.

--- cortazar/src/atoms/Filter.tsx
+++ cortazar/src/atoms/Filter.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-
 const path = 'M0 0 L5 6 L10 0'
 const pathStyle = {stroke:'#363636', fill:'transparent', strokeWidth:2 }
 
-interface iFilter { name:string, isActive:boolean, setActive():void, filters:string[], select(filter:string):void }
-export const Filter = ({name, isActive, setActive, filters, select}: iFilter) => <div className={`dropdown ${isActive ? 'is-active' : ''}`}>
+
+interface iFilter { name:string, filters:string[], select(filter:string):void }
+export const Filter = ({name, filters, select}: iFilter) => <div className={`dropdown is-hoverable`}>
     <div className='dropdown-trigger'>
-        <button className='button' aria-haspopup='true' aria-controls='dropdown-menu' onClick={setActive} style={{width:180}}>
+        <button className='button' aria-haspopup='true' aria-controls='dropdown-menu' style={{width:180}}>
             <span> { name } </span>
             <svg style={{height:24, marginLeft:'auto', width:10, paddingTop:9}}>
                 <path className='a1' d={path} style={pathStyle}></path>
