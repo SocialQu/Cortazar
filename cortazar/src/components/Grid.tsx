0939ca0f23c0b20f62e68d5ac354a48a4808afd6
Santiago8888 <santiago.marti67@gmail.com>
2021-04-25 00:28:39 -0500

    Cleaned Grid without hide filters method.

--- cortazar/src/components/Grid.tsx
+++ cortazar/src/components/Grid.tsx
@@ -20,31 +20,21 @@ const Row = ({ story }: { story: iStoryCard }) => {
 
 export const Stories = ({ stories, search }: { stories:iStoryCard[], search:string }) => {
     const isDesktop = useMediaQuery({ query: '(min-width: 1216px)' })
-
     const [ storyCards, setStoryCards ] = useState<iStoryCard[]>([])
-    const [ deactivate, setDeactivate ] = useState(false)
-
 
     useEffect(() => { setStoryCards(stories) }, [stories])
-
     const handleFilters = (filter:Filters, item:string) => {
         const filteredStories = filterStories(filter, item, stories)
         setStoryCards(filteredStories)
-        hideFilters()
     }
 
-    const hideFilters = () => {
-        setDeactivate(true)
-        setDeactivate(false)
-    }
-
-    return <div className='container' onClick={hideFilters}> 
+    return <div className='container'> 
         <h1 className='subtitle has-text-light'> 
             Showing search results for: 
             <i style={{color:'lightskyblue'}}> "{ search }" </i>
         </h1>
 
-        { isDesktop && <StoryFilters topics={[]} tags={[]} deactivate={deactivate} filterStories={handleFilters}/> }
+        { isDesktop && <StoryFilters topics={[]} tags={[]} filterStories={handleFilters}/> }
         { storyCards.filter((_, i) => i < 10).map((story, i) => <Row story={story} key={i}/>) }
     </div>
 }
